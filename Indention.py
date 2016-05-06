d86c5189d53ce90c4cd5f6bbd6bcafa728c55e51
aruntakkar <aruntakkar5@gmail.com>
2016-05-07 02:29:44 +0530

    Proper Indention Usage

---
+++ Indention.py
@@ -0,0 +1,10 @@
+# Indention or whitespaces is important in the python to structured the Code.
+# Indent your code with 4 spaces
+# Two line Break after the comment
+
+
+def spam():
+    eggs = 12
+    return eggs
+
+print(spam())
