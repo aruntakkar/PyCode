eb9a53089abc3e7ceae241a1defc2a9f14fa775c
aruntakkar <aruntakkar5@gmail.com>
2016-05-07 02:10:42 +0530

    update variable example

--- variables.py
+++ variables.py
@@ -1,3 +1,10 @@
-# Variable stores a piece of data gives it a specific name
+#my_int is set to 7 and change the value to 3 what will happen when we print my_int?
 
-my_variable = 10
+my_int = 7
+
+# changeing the value of my_int = 3
+
+my_int = 3
+
+# printing the my_int
+print(my_int)
