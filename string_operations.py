1d6fc0fe9b8b419dd2bc68714e06c59541bfd11b
aruntakkar <aruntakkar5@gmail.com>
2016-05-07 12:25:07 +0530

    string operations

---
+++ string_operations.py
@@ -0,0 +1,19 @@
+# print simply display your code in console
+
+# print a string
+print("Welcome to Python 101")
+
+# print a variable
+my_machine_goes = "Ping!"
+
+print(my_machine_goes)
+
+# printing the strings with concatenation
+print("I " + "love " + "Python")
+
+"""
+ Sometimes you need to combine a string with something that isn't string.
+ In order to do that, you have to convert the non-string into a string.
+"""
+# Here str converts non strings to strings
+print("I have " + str(2) + " coconuts!")
