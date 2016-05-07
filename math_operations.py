3516fcab06437701037c09afbae596efdb33c2e9
aruntakkar <aruntakkar5@gmail.com>
2016-05-07 10:05:43 +0530

    math operation with proper indention

---
+++ math_operations.py
@@ -0,0 +1,19 @@
+# Set the variable count_to equal to sum of two big numbers
+
+# addition
+addition = 72 + 33
+count_to = addition
+
+# subtraction
+subtraction = 108 - 204
+
+# multiplication
+multiplication = 108 * 0.5
+
+# division
+division = 108 / 9
+
+# percentage
+percentage = 100 * 10/100
+
+print(count_to)
