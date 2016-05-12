d3f2a8e55a1ca8f0ed96f4d1616b3fb65c5f28ba
aruntakkar <aruntakkar5@gmail.com>
2016-05-12 23:54:07 +0530

    all about list

--- list.py
+++ list.py
@@ -11,17 +11,26 @@ print(players[0])
 
 print(players[1])
 
+# Suppose you extend list player with the new data that list would be same
+print(players + [5, 6, 7])
+print(players)
+
+# You can extend the list with append
+players.append(120)
+print(players)
+
 # Extend the list with (list without name) here the list player would be same
 Extended_players = players + [7, 8, 9, 10]
 print(Extended_players)
 
 # Excess the element upto index 2 (so element Till 2 will be given)
-# So Here 2 is the endpoint
-# gives the element from the list value 1,2
 print(players[:2])
 
+# To Add the element till index 2 element 0
+players[:2] = [0, 0]
+print(players)
+
 # To Delete the element till the index 2
-# Create an empty list till the index you want to delete
 players[:2] = []
 
 # print the list players with the deleted elements
