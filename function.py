56a1262561984fffe627fec123a2a88a4128a48a
aruntakkar <aruntakkar5@gmail.com>
2016-05-14 17:09:13 +0530

    arguments, function, return code

--- function.py
+++ function.py
@@ -11,9 +11,3 @@ beef()
 bitcoin_to_usd(3.85)
 bitcoin_to_usd(1)
 bitcoin_to_usd(13)
-
-"""
-The statement return[expression] exists a function, optionally passing back an
-expression to the caller. A return statement with no arguments is the same as
-retrun None
-"""
