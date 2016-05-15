2b86bf0d1353f1a4bc001196187370a8027256d8
aruntakkar <aruntakkar5@gmail.com>
2016-05-15 21:17:10 +0530

    correct the indention in crawler

--- web_Crawler/flipkartcrawler.py
+++ web_Crawler/flipkartcrawler.py
@@ -3,13 +3,13 @@ from bs4 import BeautifulSoup
 
 
 def get_product_info():
-        url = "http://www.flipkart.com/all/~best-selling-mobile-laptop-accessories/pr?sid=all"
-        source_code = requests.get(url)
-        plain_text = source_code.text
-        soup = BeautifulSoup(plain_text, 'html.parser')
-        for link in soup.findAll('a', {'class': 'pu-image'}):
-            href = "http://flipkart.com" + link.get('href')
-            get_single_item_data(href)
+    url = "http://www.flipkart.com/all/~best-selling-mobile-laptop-accessories/pr?sid=all"
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, 'html.parser')
+    for link in soup.findAll('a', {'class': 'pu-image'}):
+        href = "http://flipkart.com" + link.get('href')
+        get_single_item_data(href)
 
 
 def get_single_item_data(item_url):
@@ -18,6 +18,5 @@ def get_single_item_data(item_url):
     soup = BeautifulSoup(plain_text, 'html.parser')
     for item_name in soup.findAll('h1', {'class': 'title'}):
         print(item_name.string)
-
 
 get_product_info()
