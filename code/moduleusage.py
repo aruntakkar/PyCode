b1ab92574b6e31de35728a751f38f4110facfd31
aruntakkar <aruntakkar5@gmail.com>
2016-05-16 17:40:59 +0530

    code directory commit is removed

--- code/moduleusage.py
+++
@@ -1,9 +0,0 @@
-# importing the module build by me firstmodule
-import firstmodule
-# importing the random module to generate the random number
-import random
-
-firstmodule.myname()
-
-x = random.randrange(1, 1000)
-print(x)
