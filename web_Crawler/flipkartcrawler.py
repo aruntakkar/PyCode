3232f1d15fc1345f249765bd66b609b992a0ceeb
aruntakkar <aruntakkar5@gmail.com>
2016-05-16 09:22:51 +0530

    flipkart crawler with product name and seller information with product price

--- web_Crawler/flipkartcrawler.py
+++ web_Crawler/flipkartcrawler.py
@@ -7,6 +7,7 @@ def get_product_info():
     source_code = requests.get(url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, 'html.parser')
+
     for link in soup.findAll('a', {'class': 'pu-image'}):
         href = "http://flipkart.com" + link.get('href')
         get_single_item_data(href)
@@ -16,7 +17,16 @@ def get_single_item_data(item_url):
     source_code = requests.get(item_url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, 'html.parser')
+
     for item_name in soup.findAll('h1', {'class': 'title'}):
         print(item_name.string)
+
+    for sellers_information in soup.findAll('a', {'class': 'seller-name'}):
+        seller_name = sellers_information.string
+        print(seller_name, " ", end="")
+
+    for selling_information in soup.findAll('span', {'class': 'selling-price'}):
+        selling_price = selling_information.string
+        print(selling_price, " ", end="")
 
 get_product_info()
