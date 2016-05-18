88c665c0caef3a12524c6fe4939481a5a8f217b5
aruntakkar <aruntakkar5@gmail.com>
2016-05-18 21:34:38 +0530

    dictionary operations/lambda /pillow libray usage

---
+++ code/lambda.py
@@ -0,0 +1,6 @@
+# lamdpa is a small function that don't have name
+# one time use small function
+
+answer = lambda x: x*7
+
+print(answer(5))
---
+++ code/pillow_library/imagespillow.py
@@ -0,0 +1,6 @@
+from PIL import Image
+
+img = Image.open("485.jpg")
+print(img.size)
+print(img.format)
+
