a8e3170dc9727e9e0ccc9dcc7baa688527d04cdf
aruntakkar <aruntakkar5@gmail.com>
2016-05-21 13:09:59 +0530

    realated to for loop usage

---
+++ code/codecademy/forusage.py
@@ -0,0 +1,17 @@
+""" for usage on the list"""
+names = ["Adam", "Alex", "Mariah", "Martine", "Columbus"]
+
+for name in names:
+    print(name)
+
+""" for Usage on the Dictionary"""
+
+webster = {
+    "Aardvark": "A star of a popular children's cartoon show.",
+    "Baa": "The sound a goat makes.",
+    "Carpet": "Goes on the floor.",
+    "Dab": "A small amount."
+}
+
+for key in webster:
+    print(webster[key])
