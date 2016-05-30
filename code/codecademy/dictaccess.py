55ad1d506f2b156705b20711f26b995b77e22efa
aruntakkar <aruntakkar5@gmail.com>
2016-05-30 14:07:03 +0530

    Related to dict | examstatistics

---
+++ code/codecademy/dictaccess.py
@@ -0,0 +1,9 @@
+my_dict = {
+    "name": "Arun",
+    "Age": 24,
+    "Profession": "coder"
+}
+
+print my_dict.items()
+print my_dict.keys()
+print my_dict.values()
--- code/codecademy/examstatistics.py
+++ code/codecademy/examstatistics.py
@@ -3,7 +3,7 @@ grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
 
 def print_grades(grades):
     for grade in grades:
-        print grade
+        print(grade)
 
 
 def grades_sum(grades):
@@ -23,11 +23,20 @@ def grades_variance(scores):
     average = grades_average(scores)
     variance = 0
     for score in scores:
-        # The formula of variance is Sigma squared equals the sum of
+        # The formula of population variance is sigma squared equals the sum of
         # x minus the mean squared divided by n
         variance += (average - score) ** 2
         actual_variance = variance / float(len(scores))
-    print actual_variance
     return actual_variance
 
-grades_variance(grades)
+
+def grades_std_deviation(variance):
+    return variance ** 0.5
+
+variance = grades_variance(grades)
+
+print_grades(grades)
+print(grades_sum(grades))
+print(grades_average(grades))
+print(grades_variance(grades))
+print(grades_std_deviation(variance))
