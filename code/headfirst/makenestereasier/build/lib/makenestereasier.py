e44164e364c7e9e7973746b30a0915b4c39de3f8
aruntakkar <aruntakkar5@gmail.com>
2016-06-06 14:29:30 +0530

    module new version setups

--- code/headfirst/makenestereasier/build/lib/makenestereasier.py
+++ code/headfirst/makenestereasier/build/lib/makenestereasier.py
@@ -5,15 +5,22 @@
 """
 
 
-def print_lol(the_list):
+def print_lol(the_list, level=0):
     """
-        This function takes a possitional argument "the_list", which is any
-        Python list(of possibly nested list) Each
-        data from nested list will be (recursively)
-        printed to screen on its own line.
+        This function takes a possitional argument "the_list",
+        which is any Python list(of possibly nested list) Each
+        data from nested list will be (recursively)printed to
+        screen on its own line.
+
+        A Second Argument called "level" is used to
+        insert to tab-stops when a (nested-list) is encountered
     """
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item)
+            print_lol(each_item, level+1)
         else:
+            # Use the Value of "level" to Control how
+            # Many tabs-stops are used.
+            for tab_stop in range(level):
+                print("\t", end="")
             print(each_item)
