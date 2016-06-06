bd87ff1d1c763ab84b3ca837aa82a7efbc6db2ec
aruntakkar <aruntakkar5@gmail.com>
2016-06-06 18:56:51 +0530

    module 1.4.0 is pushed

--- code/headfirst/makenestereasier/build/lib/makenestereasier.py
+++ code/headfirst/makenestereasier/build/lib/makenestereasier.py
@@ -5,7 +5,7 @@
 """
 
 
-def print_lol(the_list, level=0):
+def print_lol(the_list, indent=False, level=0):
     """
         This function takes a possitional argument "the_list",
         which is any Python list(of possibly nested list) Each
@@ -14,13 +14,19 @@ def print_lol(the_list, level=0):
 
         A Second Argument called "level" is used to
         insert to tab-stops when a (nested-list) is encountered
+
+        Indent argument is used to set the priority for indention
+        # if indent is (True) than make the indent.
+        # if indent is (False) than not.
     """
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item, level+1)
+            print_lol(each_item, indent, level+1)
         else:
-            # Use the Value of "level" to Control how
-            # Many tabs-stops are used.
-            for tab_stop in range(level):
-                print("\t", end="")
+            # specify when indenting is required by if condition
+            if indent:
+                # Use the Value of "level" to Control how
+                # Many tabs-stops are used.
+                for tab_stop in range(level):
+                    print("\t", end="")
             print(each_item)
--- code/headfirst/makenestereasier/setup.py
+++ code/headfirst/makenestereasier/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='makenestereasier',
-    version='1.2.0',
+    version='1.4.0',
     py_modules=['makenestereasier'],
     author='arun',
     author_email='aruntakkar5@gmail.com',
