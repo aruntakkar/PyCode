ae8faeb596e087b26d7f0e48d071536e6f9312be
aruntakkar <aruntakkar5@gmail.com>
2016-06-18 17:56:06 +0530

    model part is done

--- code/headfirst/chapter7/athletemodel.py
+++ code/headfirst/chapter7/athletemodel.py
@@ -8,50 +8,55 @@ def get_coach_data(filename):
             data = file.readline()
         temp1 = data.strip().split(",")
 
-        return AthleteList(temp1.pop(0), temp1.pop(0), temp1)
+        return(AthleteList(temp1.pop(0), temp1.pop(0), temp1))
 
     except IOError as err:
-        print("File error " + str(err))
+        print("File error (get_coach_data): " + str(err))
 
 
 def put_to_store(files_list):
     all_athletes = {}
 
-    for file in files_list:
-        ath = get_coach_data(file)
+    for each_file in files_list:
+        ath = get_coach_data(each_file)
         all_athletes[ath.name] = ath
 
     try:
-        with open("athlete.pickle", "wb") as atpf:
-            pickle.dump("all_athletes", atpf)
+        with open("athletes.pickle", "wb") as atpf:
+            pickle.dump(all_athletes, atpf)
 
     except IOError as err:
         print("File error(put_and_store):" + str(err))
 
-    return (all_athletes)
+    return(all_athletes)
 
 
 def get_from_store():
     all_athletes = {}
 
     try:
-        with open("athlete.pickle", "rb") as atpf:
-            pickle.load(atpf)
+        with open("athletes.pickle", "rb") as atpf:
+            all_athletes = pickle.load(atpf)
 
     except IOError as err:
         print("File error (get_from_store): " + str(err))
 
-    return (all_athletes)
+    return(all_athletes)
 
-print("--for checking the new functionality--")
 
 the_files = ['sarah2.txt', 'james2.txt', 'mikey2.txt', 'julie2.txt']
 
 data = put_to_store(the_files)
 
-
-print("To Access the data, Use the dictionary returned by put_to_store()")
+print("-To Access the data, Use the dictionary returned by put_to_store()-")
 
 for each_athlete in data:
     print(data[each_athlete].name + ' ' +
           data[each_athlete].dob)
+
+print("-To Checking the get_from_store() functionality to unpickle the data-")
+
+data_copy = get_from_store()
+
+for each_athlete in data_copy:
+    print(data_copy[each_athlete].name + ' ' + data_copy[each_athlete].dob)
