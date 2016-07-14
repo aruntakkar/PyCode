a7e8de6a334203a257035c172d9708ad8e4ed04b
aruntakkar <aruntakkar5@gmail.com>
2016-07-14 22:32:12 +0530

    deleted the reference app

--- code/headfirst/chapter10/simpleform/simpleform.py
+++ code/headfirst/chapter10/simpleform/simpleform.py
@@ -1,35 +1,9 @@
-# from google.appengine.ext.webapp import template
-# from google.appengine.ext.db import djangoforms
-# import birthDB
-
-
-# class BirthDetailsForm(djangoforms.ModelForm):
-
-#     class Meta:
-#         model = birthDB.BirthDetails
-
-#     html = template.render("templates/header.html",
-#                            {"title": "Provide your birth details"})
-
-#     html = html + template.render("templates/form_start.html", {})
-
-#     # Using your new class to generate your form
-#     # By adding the argument names are generated according to model properties.
-#     html = html + str(BirthDetailsForm(auto_id=False))
-
-#     html = html + template.render("templates/form_end.html",
-#                                   {"sub_title": "Submit Details"})
-
-#     html = html + template.render("templates/footer.html", {"links": ""})
-
 import wsgiref.handlers
 
 from google.appengine.ext import webapp
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
-
 from google.appengine.ext.db import djangoforms
-
 import birthDB
 
 
@@ -42,14 +16,21 @@ class BirthDetailsForm(djangoforms.ModelForm):
 class SimpleInput(webapp.RequestHandler):
 
     def get(self):
-        html = template.render('templates/header.html',
-                               {'title': 'Provide your birth details'})
-        html = html + template.render('templates/form_start.html', {})
+        html = template.render("templates/header.html",
+                               {"title": "Provide your birth details"})
+
+        html = html + template.render("templates/form_start.html", {})
+
+        # Using your new class to generate your form
+        # By adding the argument names are generated according to model
+        # properties.
         html = html + str(BirthDetailsForm(auto_id=False))
+
+        html = html + template.render("templates/form_end.html",
+                                      {"sub_title": "Submit Details"})
+
         html = html + \
-            template.render('templates/form_end.html',
-                            {'sub_title': 'Submit Details'})
-        html = html + template.render('templates/footer.html', {'links': ''})
+            template.render("templates/footer.html", {"links": ""})
         self.response.out.write(html)
 
 
--- code/headfirst/chapter10/webapp-chapter10-simpleform/settings.py
+++
@@ -1,9 +0,0 @@
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'America/Los_Angeles'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
